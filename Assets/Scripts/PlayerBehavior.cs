bf4f58e27ed82136ba5faeab850ff4fbe5dd0606
bootylicious9000 <148596877+bootylicious9000@users.noreply.github.com>
2024-04-18 12:14:50 -0400

    everythin

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+    public float MoveSpeed = 10f;
+    public float RotateSpeed = 75f;
+    private float _vInput;
+    private float _hInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vInput = Input.GetAxis("Vertical") * MoveSpeed;
+        _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+
+        this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+    }
+}
